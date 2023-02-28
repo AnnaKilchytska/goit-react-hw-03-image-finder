a1c7e7ad4d3a98e01e13dc4433c25a9227b1ceb7
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-03-01 00:31:28 +0200

    fix modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,17 +9,29 @@ class Modal extends Component {
   };
 
   componentDidMount() {
-    window.addEventListener('keydown', e => {
-      if (e.code === 'Escape') {
-        console.log('escape pressed');
-        this.props.onClose();
-      }
-    });
+    window.addEventListener('keydown', this.handleKeyDown);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      console.log('escape pressed');
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
 
   render() {
     return (
-      <div className="Overlay">
+      <div className="Overlay" onClick={this.handleBackdropClick}>
         <div className="Modal">
           <img src={this.props.image} alt="" />
         </div>
