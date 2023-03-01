5af315e93f9e8f4739ad36b833d91d5ab1b411c5
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-03-01 11:30:34 +0200

    fix bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,115 @@
 import React, { Component } from 'react';
 import ImageGallery from './ImageGallery';
 import Searchform from './Searchbar';
+import requestImages from '../services/api';
+import Button from './Button';
 
 class App extends Component {
   state = {
     request: '',
+    images: [],
+    status: 'idle',
+    largeImageURL: '',
+    page: 1,
+    showBtn: false,
   };
+
+  async componentDidUpdate(prevProps, prevState) {
+    try {
+      if (prevState.request !== this.state.request) {
+        console.log('new request', this.state.request);
+        console.log('previous image', prevState.request);
+
+        this.setState({ status: 'pending' });
+
+        const result = await requestImages(this.state.request, this.state.page);
+
+        console.log(result);
+        const { data: allData } = result;
+        const { hits: images } = allData;
+
+        this.setState({ images });
+
+        console.log('images after first request', images);
+
+        if (result.status === 200) {
+          this.showLoadMoreButton(allData);
+          this.setState({ status: 'success' });
+        }
+      }
+
+      if (
+        prevState.page !== this.state.page &&
+        prevState.request === this.state.request
+      ) {
+        console.log('prev state page', prevState.page);
+        console.log('this state page', this.state.page);
+
+        const newData = await requestImages(
+          this.state.request,
+          this.state.page
+        );
+
+        const { data: images } = newData;
+        const { hits: allImages } = images;
+
+        console.log('new images', allImages);
+        if (newData.status === 200) {
+          this.showLoadMoreButton(images);
+          this.setState({
+            images: [...prevState.images, ...allImages],
+            status: 'success',
+          });
+
+          console.log('new state', this.state.images);
+        }
+      }
+    } catch (error) {
+      this.setState({ status: 'error' });
+    }
+  }
 
   handleFormSubmit = image => {
     console.log(image);
-    this.setState({ request: image });
+    this.setState({
+      request: image,
+      images: [],
+      status: 'idle',
+      largeImageURL: '',
+      page: 1,
+      showBtn: false,
+    });
+    console.log(this.state.images);
+  };
+
+  handleButtonClick = () => {
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
+    });
+  };
+
+  showLoadMoreButton = result => {
+    console.log('result of search', result);
+
+    const pagesCount = Math.ceil(result.totalHits / 12);
+    console.log(pagesCount);
+    if (this.state.page < pagesCount) {
+      this.setState({ showBtn: true });
+    } else if (this.state.page === pagesCount) {
+      this.setState({ showBtn: false });
+    }
   };
 
   render() {
     return (
       <div>
         <Searchform onSubmit={this.handleFormSubmit} />
-        <ImageGallery imageRequest={this.state.request} />
+        <ImageGallery
+          images={this.state.images}
+          status={this.state.status}
+          page={this.state.page}
+        />
+        {this.state.showBtn && <Button onClick={this.handleButtonClick} />}
       </div>
     );
   }
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,23 +1,22 @@
 import PropTypes from 'prop-types';
 
 import React, { Component } from 'react';
-import requestImages from '../services/api';
-import Button from './Button';
 import ImageGalleryItem from './ImageGalleryItem';
 import Loader from './Loader';
 import Modal from './Modal';
 
 class ImageGallery extends Component {
   state = {
-    images: [],
-    status: 'idle',
-    largeImageURL: '',
-    page: 1,
+    // images: [],
+    // status: 'idle',
+    // largeImageURL: '',
+    // page: 1,
     modalOpen: false,
   };
 
   static propTypes = {
-    imageRequest: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.object).isRequired,
+    status: PropTypes.string.isRequired,
   };
 
   handleModalClick = url => {
@@ -30,70 +29,70 @@ class ImageGallery extends Component {
     this.setState({ modalOpen: false });
   };
 
-  handleButtonClick = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
-  };
+  // handleButtonClick = () => {
+  //   this.setState(prevState => {
+  //     return { page: prevState.page + 1 };
+  //   });
+  // };
 
-  async componentDidUpdate(prevProps, prevState) {
-    try {
-      if (prevProps.imageRequest !== this.props.imageRequest) {
-        console.log('new request', this.props.imageRequest);
-        console.log('previous image', prevProps.imageRequest);
+  // async componentDidUpdate(prevProps, prevState) {
+  //   try {
+  //     if (prevProps.imageRequest !== this.props.imageRequest) {
+  //       console.log('new request', this.props.imageRequest);
+  //       console.log('previous image', prevProps.imageRequest);
 
-        this.setState({ status: 'pending', page: 1 });
+  //       this.setState({ status: 'pending', page: 1 });
 
-        const result = await requestImages(
-          this.props.imageRequest,
-          this.state.page
-        );
+  //       const result = await requestImages(
+  //         this.props.imageRequest,
+  //         this.state.page
+  //       );
 
-        console.log(result);
-        const { data: allData } = result;
-        const { hits: images } = allData;
+  //       console.log(result);
+  //       const { data: allData } = result;
+  //       const { hits: images } = allData;
 
-        this.setState({ images });
+  //       this.setState({ images });
 
-        console.log(images);
-        //   console.log('state', this.state.images);
-        if (result.status === 200) {
-          this.setState({ status: 'success' });
-        }
-      }
+  //       console.log(images);
+  //       //   console.log('state', this.state.images);
+  //       if (result.status === 200) {
+  //         this.setState({ status: 'success' });
+  //       }
+  //     }
 
-      if (prevState.page !== this.state.page) {
-        console.log('prev state page', prevState.page);
-        console.log('this state page', this.state.page);
+  //     if (prevState.page !== this.state.page) {
+  //       console.log('prev state page', prevState.page);
+  //       console.log('this state page', this.state.page);
 
-        const newData = await requestImages(
-          this.props.imageRequest,
-          this.state.page
-        );
+  //       const newData = await requestImages(
+  //         this.props.imageRequest,
+  //         this.state.page
+  //       );
 
-        const { data: images } = newData;
-        const { hits: allImages } = images;
+  //       const { data: images } = newData;
+  //       const { hits: allImages } = images;
 
-        console.log('new images', allImages);
-        if (newData.status === 200) {
-          this.setState({
-            images: [...prevState.images, ...allImages],
-            status: 'success',
-          });
+  //       console.log('new images', allImages);
+  //       if (newData.status === 200) {
+  //         this.setState({
+  //           images: [...prevState.images, ...allImages],
+  //           status: 'success',
+  //         });
 
-          console.log('new state', this.state.images);
-        }
-      }
-    } catch (error) {
-      this.setState({ status: 'error' });
-    }
-  }
+  //         console.log('new state', this.state.images);
+  //       }
+  //     }
+  //   } catch (error) {
+  //     this.setState({ status: 'error' });
+  //   }
+  // }
 
   render() {
-    const { status, images } = this.state;
+    const { status, images } = this.props;
 
     if (status === 'idle') {
-      return <h1>Start searching for images</h1>;
+      return <h1 className="message">Start searching for images</h1>;
     }
 
     if (status === 'pending') {
@@ -101,7 +100,9 @@ class ImageGallery extends Component {
     }
 
     if (status === 'error' || images.length === 0) {
-      return <h1>Oops, something went wrong! Try again!</h1>;
+      return (
+        <h1 className="message">Oops, something went wrong! Try again!</h1>
+      );
     }
 
     if (status === 'success') {
@@ -114,7 +115,7 @@ class ImageGallery extends Component {
             />
           )}
           <ul className="ImageGallery">
-            {this.state.images.map(image => {
+            {this.props.images.map(image => {
               return (
                 <ImageGalleryItem
                   key={image.id}
@@ -126,8 +127,6 @@ class ImageGallery extends Component {
               );
             })}
           </ul>
-
-          <Button onClick={this.handleButtonClick} />
         </>
       );
     }
