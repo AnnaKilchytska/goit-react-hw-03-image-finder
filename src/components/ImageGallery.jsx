17ba7a26f193042ee277b2a269719fbe64a359f0
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-03-01 11:51:47 +0200

    small cleaning

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -7,10 +7,6 @@ import Modal from './Modal';
 
 class ImageGallery extends Component {
   state = {
-    // images: [],
-    // status: 'idle',
-    // largeImageURL: '',
-    // page: 1,
     modalOpen: false,
   };
 
@@ -28,65 +24,6 @@ class ImageGallery extends Component {
   handleModalClose = () => {
     this.setState({ modalOpen: false });
   };
-
-  // handleButtonClick = () => {
-  //   this.setState(prevState => {
-  //     return { page: prevState.page + 1 };
-  //   });
-  // };
-
-  // async componentDidUpdate(prevProps, prevState) {
-  //   try {
-  //     if (prevProps.imageRequest !== this.props.imageRequest) {
-  //       console.log('new request', this.props.imageRequest);
-  //       console.log('previous image', prevProps.imageRequest);
-
-  //       this.setState({ status: 'pending', page: 1 });
-
-  //       const result = await requestImages(
-  //         this.props.imageRequest,
-  //         this.state.page
-  //       );
-
-  //       console.log(result);
-  //       const { data: allData } = result;
-  //       const { hits: images } = allData;
-
-  //       this.setState({ images });
-
-  //       console.log(images);
-  //       //   console.log('state', this.state.images);
-  //       if (result.status === 200) {
-  //         this.setState({ status: 'success' });
-  //       }
-  //     }
-
-  //     if (prevState.page !== this.state.page) {
-  //       console.log('prev state page', prevState.page);
-  //       console.log('this state page', this.state.page);
-
-  //       const newData = await requestImages(
-  //         this.props.imageRequest,
-  //         this.state.page
-  //       );
-
-  //       const { data: images } = newData;
-  //       const { hits: allImages } = images;
-
-  //       console.log('new images', allImages);
-  //       if (newData.status === 200) {
-  //         this.setState({
-  //           images: [...prevState.images, ...allImages],
-  //           status: 'success',
-  //         });
-
-  //         console.log('new state', this.state.images);
-  //       }
-  //     }
-  //   } catch (error) {
-  //     this.setState({ status: 'error' });
-  //   }
-  // }
 
   render() {
     const { status, images } = this.props;
