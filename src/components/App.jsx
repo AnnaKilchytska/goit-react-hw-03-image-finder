cce5f0e48eee0f4e22984194dd5375999c2b88f5
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-03-02 10:56:52 +0200

    fix rendering of images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import ImageGallery from './ImageGallery';
 import Searchform from './Searchbar';
 import requestImages from '../services/api';
 import Button from './Button';
+import Loader from './Loader';
 
 class App extends Component {
   state = {
@@ -20,54 +21,25 @@ class App extends Component {
         prevState.request !== this.state.request ||
         prevState.page !== this.state.page
       ) {
-        // console.log('new request', this.state.request);
-        // console.log('previous image', prevState.request);
-
         this.setState({ status: 'pending' });
 
         const result = await requestImages(this.state.request, this.state.page);
 
-        // console.log(result);
         const { data: allData } = result;
         const { hits: images } = allData;
 
-        // this.setState({ images });
-
-        // console.log('images after first request', images);
-        this.setState({
-          images:
-            this.state.page === 1 ? images : [...prevState.images, ...images],
-          status: 'success',
-          showBtn: this.state.page < Math.ceil(allData.totalHits / 12),
-        });
+        if (images.length === 0) {
+          this.setState({ status: 'error' });
+          return;
+        } else {
+          this.setState({
+            images:
+              this.state.page === 1 ? images : [...prevState.images, ...images],
+            status: 'success',
+            showBtn: this.state.page < Math.ceil(allData.totalHits / 12),
+          });
+        }
       }
-
-      // if (
-      //   prevState.page !== this.state.page &&
-      //   prevState.request === this.state.request
-      // ) {
-      //   // console.log('prev state page', prevState.page);
-      //   // console.log('this state page', this.state.page);
-
-      //   const newData = await requestImages(
-      //     this.state.request,
-      //     this.state.page
-      //   );
-
-      //   const { data: images } = newData;
-      //   const { hits: allImages } = images;
-
-      //   // console.log('new images', allImages);
-
-      //   // this.showLoadMoreButton(images);
-      //   this.setState({
-      //     images: [...prevState.images, ...allImages],
-      //     status: 'success',
-      //     showBtn: this.state.page < Math.ceil(images.totalHits / 12),
-      //   });
-
-      //   console.log('new state', this.state.images);
-      // }
     } catch (error) {
       this.setState({ status: 'error' });
     }
@@ -92,17 +64,27 @@ class App extends Component {
   };
 
   render() {
+    const { status, images, page } = this.state;
+
     return (
-      <div>
+      <>
         <Searchform onSubmit={this.handleFormSubmit} />
-        <ImageGallery
-          images={this.state.images}
-          status={this.state.status}
-          page={this.state.page}
-        />
-        {this.state.showBtn && <Button onClick={this.handleButtonClick} />}
-      </div>
+        {status === 'idle' && (
+          <h1 className="message">Start searching for images</h1>
+        )}
+        {status === 'error' && (
+          <h1 className="message">Oops, something went wrong. Try again!</h1>
+        )}
+
+        {status === 'pending' && <Loader />}
+
+        <div>
+          <ImageGallery images={images} status={status} page={page} />
+          {this.state.showBtn && <Button onClick={this.handleButtonClick} />}
+        </div>
+      </>
     );
+    // }
   }
 }
 
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 
 import React, { Component } from 'react';
 import ImageGalleryItem from './ImageGalleryItem';
-import Loader from './Loader';
 import Modal from './Modal';
 
 class ImageGallery extends Component {
@@ -26,47 +25,29 @@ class ImageGallery extends Component {
   };
 
   render() {
-    const { status, images } = this.props;
-
-    if (status === 'idle') {
-      return <h1 className="message">Start searching for images</h1>;
-    }
-
-    if (status === 'pending') {
-      return <Loader />;
-    }
-
-    if (status === 'error' || images.length === 0) {
-      return (
-        <h1 className="message">Oops, something went wrong! Try again!</h1>
-      );
-    }
-
-    if (status === 'success') {
-      return (
-        <>
-          {this.state.modalOpen === true && (
-            <Modal
-              image={this.state.largeImageURL}
-              onClose={this.handleModalClose}
-            />
-          )}
-          <ul className="ImageGallery">
-            {this.props.images.map(image => {
-              return (
-                <ImageGalleryItem
-                  key={image.id}
-                  url={image.webformatURL}
-                  alt={image.tags}
-                  large={image.largeImageURL}
-                  onClick={() => this.handleModalClick(image.largeImageURL)}
-                />
-              );
-            })}
-          </ul>
-        </>
-      );
-    }
+    return (
+      <>
+        {this.state.modalOpen === true && (
+          <Modal
+            image={this.state.largeImageURL}
+            onClose={this.handleModalClose}
+          />
+        )}
+        <ul className="ImageGallery">
+          {this.props.images.map(image => {
+            return (
+              <ImageGalleryItem
+                key={image.id}
+                url={image.webformatURL}
+                alt={image.tags}
+                large={image.largeImageURL}
+                onClick={() => this.handleModalClick(image.largeImageURL)}
+              />
+            );
+          })}
+        </ul>
+      </>
+    );
   }
 }
 
